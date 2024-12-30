3eb4b74f9a89e4f75b343bb03ad5f5ddddf7be22
Vladislav Lotockij <vladislavlotockij1@gmail.com>
2024-12-30 18:34:33 +0200

    Add categories controller and view

---
+++ resources/views/admin/includes/sidebar.blade.php
@@ -0,0 +1,15 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+
+    <div class="sidebar">
+        <ul class="pt-3 nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="navbar-icon fa-duotone fa-solid fa-bars"></i>
+                    <p>
+                        Категории
+                    </p>
+                </a>
+            </li>
+        </ul>
+    </div>
+</aside>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,6 +19,9 @@ Route::group(['namespace' => 'Main'], function () {
 
 Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function () {
     Route::group(['namespace' => 'Main'], function () {
+        Route::get('/', 'IndexController');
+    });
+    Route::group(['namespace' => 'Category', 'prefix' => 'categories'], function () {
         Route::get('/', 'IndexController');
     });
 });
