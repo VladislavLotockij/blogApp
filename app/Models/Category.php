163eb8f0086517c657fcff439cd5618a54fb87c1
Vladislav Lotockij <vladislavlotockij1@gmail.com>
2024-12-31 18:35:28 +0200

    added column soft_delete to categories table

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $table = 'categories';
     protected $guarded = false;
