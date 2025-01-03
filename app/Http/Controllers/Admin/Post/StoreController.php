000c91de3c941cb5921a0a72ce257881f96c4847
Vladislav Lotockij <vladislavlotockij1@gmail.com>
2025-01-03 14:36:54 +0200

    Added the ability to link tags

--- app/Http/Controllers/Admin/Post/StoreController.php
+++ app/Http/Controllers/Admin/Post/StoreController.php
@@ -5,16 +5,24 @@ namespace App\Http\Controllers\Admin\Post;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\Post\StoreRequest;
 use App\Models\Post;
+use Exception;
 use Illuminate\Support\Facades\Storage;
 
 class StoreController extends Controller
 {
     public function __invoke(StoreRequest $request)
     {
-        $data = $request->validated();
-        $data['preview_image'] = Storage::putFile('/images', $data['preview_image']);
-        $data['main_image'] = Storage::putFile('/images', $data['main_image']);
-        Post::firstOrCreate($data);
+        try {
+            $data = $request->validated();
+            $tagsIds = $data['tag_ids'];
+            unset($data['tag_ids']);
+            $data['preview_image'] = Storage::putFile('/images', $data['preview_image']);
+            $data['main_image'] = Storage::putFile('/images', $data['main_image']);
+            $post = Post::firstOrCreate($data);
+            $post->tags()->attach($tagsIds);
+        } catch(Exception $e) {
+            aborT(404);
+        }
         return redirect() -> route('admin.post.index');
     }
 }
