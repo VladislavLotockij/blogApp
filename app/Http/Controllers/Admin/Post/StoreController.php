357130f653b00bc6c6de3fbcd4c50674f553e22d
Vladislav Lotockij <vladislavlotockij1@gmail.com>
2025-01-03 19:57:34 +0200

    implemented update for posts, editing form, image display

--- app/Http/Controllers/Admin/Post/StoreController.php
+++ app/Http/Controllers/Admin/Post/StoreController.php
@@ -16,8 +16,8 @@ class StoreController extends Controller
             $data = $request->validated();
             $tagsIds = $data['tag_ids'];
             unset($data['tag_ids']);
-            $data['preview_image'] = Storage::putFile('/images', $data['preview_image']);
-            $data['main_image'] = Storage::putFile('/images', $data['main_image']);
+            $data['preview_image'] = Storage::disk('public')->put('/images', $data['preview_image']);
+            $data['main_image'] = Storage::disk('public')->put('/images', $data['main_image']);
             $post = Post::firstOrCreate($data);
             $post->tags()->attach($tagsIds);
         } catch(Exception $e) {
--- resources/views/admin/post/edit.blade.php
+++ resources/views/admin/post/edit.blade.php
@@ -24,17 +24,81 @@
         <section class="content">
             <div class="container-fluid">
                 <div class="col-12">
-                    <form action="{{ route('admin.post.update', $post->id) }}" method="POST" class="w-25">
+                    <form action="{{ route('admin.post.update', $post->id) }}" method="POST" enctype="multipart/form-data">
                         @csrf
                         @method('PATCH')
                         <div class="form-group">
-                            <input type="text" class="form-control" name="title" placeholder="Название поста" value="{{$post->title}}">
+                            <input type="text" class="form-control" name="title" placeholder="Название поста" value="{{ $post->title }}">
                             @error('title')
-                                <div class="text-danger">Это обезательное поле</div>
+                            <div class="text-danger">Это обезательное поле</div>
                             @enderror
                         </div>
+                        <div class="form-group">
+                            <textarea id="summernote" name="content">{{ $post->content }}</textarea>
+                            @error('content')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputFile">Добавить превью</label>
+                            <div class="w-25 mb-3">
+                                <img src="{{ asset('storage/' . $post->preview_image) }}" alt="preview_image" class="w-50">
+                            </div>
+                            <div class="input-group">
+                              <div class="custom-file">
+                                <input type="file" class="custom-file-input" name="preview_image">
+                                <label class="custom-file-label">Choose file</label>
+                              </div>
+                              <div class="input-group-append">
+                                <span class="input-group-text">Загрузить</span>
+                              </div>
+                            </div>
+                            @error('preview_image')
+                            <div class="text-danger">Это обезательное поле</div>
+                            @enderror
+                          </div>
+                          <div class="form-group">
+                            <label for="exampleInputFile">Добавить главное изображение</label>
+                            <div class="w-25 mb-3">
+                                <img src="{{ asset('storage/' . $post->main_image) }}" alt="main_image" class="w-50">
+                            </div>
+                            <div class="input-group">
+                              <div class="custom-file">
+                                <input type="file" class="custom-file-input" name="main_image">
+                                <label class="custom-file-label">Choose file</label>
+                              </div>
+                              <div class="input-group-append">
+                                <span class="input-group-text">Загрузить</span>
+                              </div>
+                            </div>
+                            @error('main_image')
+                            <div class="text-danger">Это обезательное поле</div>
+                            @enderror
+                          </div>
+                          <div class="col-sm-6">
+                            <!-- select -->
+                            <div class="form-group w-50">
+                              <label>Выберите категорию</label>
+                              <select name="category_id" class="form-control">
+                                @foreach ($categories as $category)
+                                    <option value="{{ $category->id }}"
+                                    {{ $category->id == $post->category_id ? 'selected' : '' }}    
+                                    >{{ $category->title }}</option>
+                                @endforeach
+                              </select>
+                            </div>
+                          </div>
+                          <div class="form-group">
+                            <label>Теги</label>
+                            <select class="select2" multiple="multiple" name="tag_ids[]" data-placeholder="Выберите теги" style="width: 100%;">
+                                @foreach ($tags as $tag)
+                                <option {{ is_array($post->tags->pluck('id')->toArray()) && in_array($tag->id, $post->tags->pluck('id')->toArray()) ? 'selected' : ''}} value="{{ $tag->id}}">{{ $tag->title}}</option>
+                                @endforeach
+                            </select>
+                          </div>
+                        <div class="form-group">
                             <input type="submit" class="btn btn-primary" value="Обновить">
-                    </form>
+                        </div>
                     <div class="col-3 mt-4">
                         <a href="{{ route('admin.post.index') }}" class="btn btn-block btn-primary">Вернуться</a>
                     </div>
