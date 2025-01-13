006b923be0db194b44cebf5f59c83a2e568bf756
Vladislav <vladislavlotockij1@gmail.com>
2025-01-13 14:01:31 +0200

    Add display output for main

--- app/Http/Controllers/Main/IndexController.php
+++ app/Http/Controllers/Main/IndexController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers\Main;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function __invoke()
     {
-        return view('main.index');
+        $posts = Post::paginate(6);
+        return view('main.index', compact('posts'));
     }
 }
 
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 //test test test
 
 Route::group(['namespace' => 'Main'], function () {
-    Route::get('/', 'IndexController');
+    Route::get('/', 'IndexController')->name('main.index');
 });
 
 Route::group(['namespace' => 'Personal', 'prefix' => 'personal', 'middleware' => ['auth', 'verified']], function () {
