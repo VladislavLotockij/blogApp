463cba21f8631469fbd51f579e3ce9162e559612
Vladislav <vladislavlotockij1@gmail.com>
2025-01-13 18:34:55 +0200

    Updated main page route logiс

--- app/Http/Controllers/Main/IndexController.php
+++ app/Http/Controllers/Main/IndexController.php
@@ -10,9 +10,6 @@ class IndexController extends Controller
 {
     public function __invoke()
     {
-        $posts = Post::paginate(6);
-        $randomPosts = Post::inRandomOrder()->take(4)->get();
-        $likedPosts = Post::withCount('likedUsers')->orderBy('liked_users_count', 'desc')->take(4)->get();
-        return view('main.index', compact('posts', 'randomPosts', 'likedPosts'));
+        return redirect()->route('post.index');
     }
 }
